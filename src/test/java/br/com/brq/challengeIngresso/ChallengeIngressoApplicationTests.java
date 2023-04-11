4f7caafb59b8af88c088b1651578af665c5c4255
Tassia Monteiro <tassia.monteiro90@gmail.com>
2023-04-11 15:29:01 -0300

    Primeiro commit

---
+++ src/test/java/br/com/brq/challengeIngresso/ChallengeIngressoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brq.challengeIngresso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeIngressoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
